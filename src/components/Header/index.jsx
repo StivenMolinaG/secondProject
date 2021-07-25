e00269c160ee832cf5dbdd011808a10f06d517da
StivenMolinaG <stmolinag@gmail.com>
2021-07-25 17:07:37 -0500

    APIS independent without promise

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+// import { withRouter } from 'react-router-dom';
+import HeaderOption from '../headerOption';
+import headerOptions from './headerOptions.json';
+import './header.css';
+
+class Header extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            activeOption: 'home'
+        }
+    }
+    onClick = (activeOption, pathOption) => {
+        this.setState({ activeOption: activeOption });
+        this.props.history.push(pathOption);
+    }
+    render(){
+        return (
+            <header className="header">
+                <nav className="header__nav nav">
+                    <ul className="nav__list list">
+                        {
+                            headerOptions.map((hopt, index) => {
+                                return (
+                                    <HeaderOption activeOption={this.state.activeOption}
+                                        onClick={this.onClick}
+                                        optionText={hopt.optionText}
+                                        option={hopt.option}
+                                        path={hopt.path}
+                                        key={index} />
+                                );
+                            })
+                        }
+                    </ul>
+                </nav>
+            </header>
+        );
+    }
+}
+export default Header;
